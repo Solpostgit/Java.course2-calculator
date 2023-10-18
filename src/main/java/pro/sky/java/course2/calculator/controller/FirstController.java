ae84208e1c39968e2103241ae4e7ae0f428cc4c3
Solpostgit <begemot_off@mail.ru>
2023-10-18 22:31:59 +0300

    Вынес общую часть "/calculator" в аннотацию @RequestMapping на уровне класса

--- src/main/java/pro/sky/java/course2/calculator/controller/FirstController.java
+++ src/main/java/pro/sky/java/course2/calculator/controller/FirstController.java
@@ -1,11 +1,13 @@
 package pro.sky.java.course2.calculator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class FirstController {
-    @GetMapping("/calculator")
+    @GetMapping //метод 1 выполнен
     public String greetingsUser() {
         return "Добро пожаловать в калькулятор";
     }
